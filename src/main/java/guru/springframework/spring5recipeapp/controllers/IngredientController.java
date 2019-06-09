0139b46dadb56f4c3d1dccc405eb983693ee134d
minarazi <razimina76@gmail.com>
2019-06-09 15:38:52 +0430

    create class IngredientController

---
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IngredientController.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class IngredientController {
+	private final RecipeService recipeService;
+
+	public IngredientController(RecipeService recipeService) {
+		this.recipeService = recipeService;
+	}
+
+	@GetMapping
+	@RequestMapping("recipe/{recipeId}/ingredient")
+	public String listIngredients(@PathVariable String recipeId, Model model) {
+		log.debug("Getting Ingredient list for Recipe Id :" + recipeId);
+		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+		return "recipe/ingredient/list";
+
+	}
+
+}
