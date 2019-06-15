d859511b3fa946f2503f86cdb646e670075e5d5b
minarazi <razimina76@gmail.com>
2019-06-15 16:25:48 +0430

    add metod showRecipeIngredient to ingredientController

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.spring5recipeapp.services.IngredientService;
 import guru.springframework.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,9 +14,11 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class IngredientController {
 	private final RecipeService recipeService;
+	private final IngredientService ingredientService;
 
-	public IngredientController(RecipeService recipeService) {
+	public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
 		this.recipeService = recipeService;
+		this.ingredientService = ingredientService;
 	}
 
 	@GetMapping
@@ -24,6 +27,15 @@ public class IngredientController {
 		log.debug("Getting Ingredient list for Recipe Id :" + recipeId);
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 		return "recipe/ingredient/list";
+
+	}
+
+	@GetMapping
+	@RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+	public String showRecipeIngredient(@PathVariable String recipeId, @PathVariable String id, Model model) {
+		model.addAttribute("ingredient",
+				ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
+		return "recipe/ingredient/show";
 
 	}
 
