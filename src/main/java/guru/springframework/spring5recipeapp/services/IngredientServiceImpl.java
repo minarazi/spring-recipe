595f6546b0df1874439557e85c930d9891a63ea6
minarazi <razimina76@gmail.com>
2019-06-16 23:05:46 +0430

    add ingredient save serivce

--- src/main/java/guru/springframework/spring5recipeapp/services/IngredientServiceImpl.java
+++ src/main/java/guru/springframework/spring5recipeapp/services/IngredientServiceImpl.java
@@ -2,12 +2,17 @@ package guru.springframework.spring5recipeapp.services;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import guru.springframework.spring5recipeapp.commands.IngredientCommand;
+import guru.springframework.spring5recipeapp.converters.IngredientCommandToIngredient;
 import guru.springframework.spring5recipeapp.converters.IngredientToIngredientCommand;
+import guru.springframework.spring5recipeapp.domain.Ingredient;
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -16,12 +21,17 @@ public class IngredientServiceImpl implements IngredientService {
 
 	private final IngredientToIngredientCommand ingredientToIngredientCommand;
 	private final RecipeRepository recipeRepository;
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+	private final IngredientCommandToIngredient ingredientCommandToIngredient;
 
 	public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand,
-			RecipeRepository recipeRepository) {
+			RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository,
+			IngredientCommandToIngredient ingredientCommandToIngredient) {
 		super();
 		this.ingredientToIngredientCommand = ingredientToIngredientCommand;
 		this.recipeRepository = recipeRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+		this.ingredientCommandToIngredient = ingredientCommandToIngredient;
 	}
 
 	@Override
@@ -36,11 +46,45 @@ public class IngredientServiceImpl implements IngredientService {
 				.map(ingredient -> ingredientToIngredientCommand.convert(ingredient)).findFirst();
 
 		if (!ingredientCommandOptional.isPresent()) {
-			// todo impl error handeling
+			// TODO implement error handling
 			log.error("ingredient Id not found. id:" + recipeId);
 		}
 
 		return ingredientCommandOptional.get();
+	}
+
+	@Override
+	@Transactional
+	public IngredientCommand saveIngredientCommand(IngredientCommand command) {
+		Optional<Recipe> recipeOptional = recipeRepository.findById(command.getRecipeId());
+		if (!recipeOptional.isPresent()) {
+			log.error("recipe Id not found. id:" + command.getRecipeId());
+			return new IngredientCommand();
+		} else {
+			Recipe recipe = recipeOptional.get();
+			Optional<Ingredient> ingredientOptional = recipe.getIngredients().stream()
+					.filter(ingredient -> ingredient.getId().equals(command.getId())).findFirst();
+			if (ingredientOptional.isPresent()) {
+				Ingredient ingredientFound = ingredientOptional.get();
+				ingredientFound.setDescription(command.getDescription());
+				ingredientFound.setAmount(command.getAmount());
+
+				ingredientFound
+						.setUnitOfMeasure(unitOfMeasureRepository.findById(command.getUnitOfMeasureCommand().getId())
+								.orElseThrow(() -> new RuntimeException("UOM NOT FOUND")));// TODO address this
+			} else {
+				// add new ingredient
+				recipe.addIngredient(ingredientCommandToIngredient.convert(command));
+			}
+
+			Recipe savedRecipe = recipeRepository.save(recipe);
+
+			// TODO check for fail
+
+			return ingredientToIngredientCommand.convert(savedRecipe.getIngredients().stream()
+					.filter(ingredient -> ingredient.getId().equals(command.getId())).findFirst().get());
+		}
+
 	}
 
 }
