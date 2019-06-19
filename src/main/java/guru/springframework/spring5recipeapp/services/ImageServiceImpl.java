9d89eac4c7d86187e8a429ad9cca5126244bbd61
minarazi <razimina76@gmail.com>
2019-06-19 22:21:49 +0430

    add image service

--- src/main/java/guru/springframework/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,17 +1,45 @@
 package guru.springframework.spring5recipeapp.services;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+	private final RecipeRepository recipeRepository;
+
+	public ImageServiceImpl(RecipeRepository recipeRepository) {
+		super();
+		this.recipeRepository = recipeRepository;
+	}
+
 	@Override
 	public void saveImageFile(Long recipeId, MultipartFile file) {
+		try {
+			Recipe recipe = recipeRepository.findById(recipeId).get();
 
-		log.debug("Recived a file");
+			Byte[] byteObjects = new Byte[file.getBytes().length];
+
+			int i = 0;
+			for (byte b : file.getBytes()) {
+				byteObjects[i++] = b;
+			}
+			recipe.setImage(byteObjects);
+
+			recipeRepository.save(recipe);
+		} catch (IOException e) {
+			// TODO handle better
+			log.error("Error occurred", e);
+
+			e.printStackTrace();
+		}
 
 	}
 
